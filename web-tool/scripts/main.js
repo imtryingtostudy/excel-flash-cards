4571605d3282ad6e8ff9afe027faa71d05c1ef8e
Colin Regan <ryanR513@yahoo.com>
2023-08-26 00:00:25 -0400

    Added dependency scripts. Implemented file parsing for sheets & terms into parsable dicts. Added comments

--- web-tool/scripts/main.js
+++ web-tool/scripts/main.js
@@ -1,45 +1,6 @@
-// const myImage = document.querySelector("img");
+import { read, writeFileXLSX } from "./dist/xlsx.mjs";
 
-// myImage.onclick = () => {
-//     const mySrc = myImage.getAttribute("src");
-//     if (mySrc === "images/hello.jpg") {
-//         myImage.setAttribute("src", "images/goodbye.png");
-//     } else {
-//         myImage.setAttribute("src", "images/hello.jpg");
-//     }
-// };
-
-// let myButton = document.querySelector("button");
-// myButton.onclick = () => {
-//     setUserName();
-// };
-
-// let myHeading = document.querySelector("h1");
-
-// function setUserName() {
-//     const myName = prompt("Please enter your name.");
-//     if (!myName) {
-//       setUserName();
-//     } else {
-//       localStorage.setItem("name", myName);
-//       myHeading.textContent = `Mozilla is cool, ${myName}`;
-//     }
-//   }
-
-// if (!localStorage.getItem("name")) {
-//     setUserName();
-// } else {
-//     const storedName = localStorage.getItem("name");
-//     myHeading.textContent = `Mozilla is cool, ${storedName}`;
-// }
-
-import utils from "xlsx";
-import { WorkBook } from "xlsx";
-
-const notecard = {
-    'TERM': 'term',
-    'DEFINITION': 'definition'
-}
+const userFile = document.getElementsByClassName("user-file")[0];
 
 async function handleFileAsync(e) {
     const file = e.target.files[0];
@@ -47,7 +8,49 @@ async function handleFileAsync(e) {
     /* data is an ArrayBuffer */
     const workbook = XLSX.read(data);
 
-    /* DO SOMETHING WITH workbook HERE */
+    // Store how many sheets (subjects/decks) are in the file
+    const sheets = workbook.SheetNames
+
+    var decks = {};
+    // Iterate over all of our sheets
+    for (var n = 0; n < sheets.length; n++) {
+        var cards = {};
+
+        // Grab the first sheet
+        const sheetName = workbook.SheetNames[n];
+        const sheet = workbook.Sheets[sheetName];
+
+        // Initialize arrays to store column data
+        var columnAData = [];
+        var columnBData = [];
+
+        // Iterate through the rows and extract data from columns A and B
+        for (const cell in sheet) {
+            if (cell[0] === 'A' && sheet.hasOwnProperty(cell)) {
+                columnAData.push(sheet[cell].v);
+            }
+            if (cell[0] === 'B' && sheet.hasOwnProperty(cell)) {
+                columnBData.push(sheet[cell].v);
+            }
+        }
+
+        // Verify that we have an equal number of keys to values
+        if (columnAData.length === columnBData.length) {
+            for (var i = 1; i < columnAData.length; i++) {
+                // Insert each flash card pairing into our dictionary for this sheet
+                cards[columnAData[i]] = columnBData[i];
+            }
+        } else {
+            alert("Please check that every Term-Value pair is filled in")
+        }
+
+        decks[sheetName] = cards;
+    }
+
+    localStorage.setItem("decks", JSON.stringify(decks));
+    let print_deck = JSON.parse(localStorage.getItem("decks"));
+    console.log(print_deck);
 }
 
-user_flash_cards_sheet.addEventListener("change", handleFileAsync, false);
+userFile.addEventListener("change", handleFileAsync, false);
+
